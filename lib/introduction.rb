4cd3234c30d58e4b0b02b1e9c07acb2806ab529b
Christopher Watkins <chriswatkins7@yahoo.com>
2020-10-05 17:11:43 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,5 +11,5 @@ language =  "Ember.js"
 
 
 def introduction_with_language(student,language)
-  puts "Hi, my name is #{student} and I am learning in to program in #{language}."
+  puts "Hi, my name is #{student} and I am learning to program in #{language}."
 end
